39028803fffb39eae4db82d1b8993024ba8b68d3
Kağan YILMAZ <ykaan2911@gmail.com>
2023-03-22 15:59:24 +0300

    homework of OOP (simple_prg1)(Murat Kagan YILMAZ)

---
+++ src/baslangic/KaganYilmaz.java
@@ -0,0 +1,8 @@
+package baslangic;
+
+public class KaganYilmaz {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!,I am Murat Kagan YILMAZ");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Kagan
+ *
+ */
+module baslangic {
+}
